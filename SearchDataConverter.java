4809eae3f9118feb8b5abe0c909abc05560c26e1
tenoritaiga <ringbearer91@gmail.com>
2013-12-01 01:15:57 -0500

    Add a few more combo box converters

--- SearchDataConverter.java
+++ SearchDataConverter.java
@@ -13,15 +13,14 @@ package ebayproject;
 public class SearchDataConverter {
    public static String CurrencyComboboxConversion (String number) {
        String[] data = new String[] {
-               "US Dollar (USD)",
-                "Australian Dollar (AUD)",
-                "Canadian dollar (CAD)",
-                "Euro (EUR)",
-                "Indian Rupee (INR)",
-                "New Taiwan Dollar (TWD)",
-                "Pound Sterling (GBP)"
+               "USD",
+                "AUD",
+                "CAD",
+                "EUR",
+                "INR",
+                "TWD",
+                "GBP"
            };
-       
        try {
            Integer i = Integer.parseInt(number);
             return data[i];
@@ -29,4 +28,42 @@ public class SearchDataConverter {
            return data[0];
        }
    }
+   
+   public static String ConditionComboboxConversion (String number) {
+       String[] data = new String[] {
+           "New",
+           "Used",
+           "Unspecified"
+       };
+       try {
+           Integer i = Integer.parseInt(number);
+            return data[i];
+       } catch (Exception ex) {
+           return data[0];
+       }
+   }
+   
+   
+   
+   //Use this in sortOrderType, not itemFilter
+   public static String SortByComboboxConversion (String number) {
+       String[] data = new String[] {
+           "EndTimeSoonest",
+           "StartTimeNewest",
+           "PricePlusShippingLowest",
+           "PricePlusShippingHighest",
+           "CurrentPriceHighest",
+           "DistanceNearest",   //request must also include buyerPostalCode
+           "BestMatch"
+       };
+       try {
+           Integer i = Integer.parseInt(number);
+            return data[i];
+       } catch (Exception ex) {
+           return data[0];
+       }
+   }
+   
+   
+   
 }
